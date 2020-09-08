310195d553af565b51b414810acf2de2880ccbb3
cyruscchan <70585816+cyruscchan@users.noreply.github.com>
2020-09-08 17:38:42 +0800

    Colour fix

--- js/app.js
+++ js/app.js
@@ -1,27 +1,47 @@
 let speed = 20;
-let scale = 0.17; // Image scale (I work on 1080p monitor)
+let scale = 0.05; // Image scale (I work on 1080p monitor)
 let canvas;
 let ctx;
 let logoColor;
+var lastHit = 0; //UNIX time of the last hit
+//[0] = total hit, [1] = corner hit, [2] = Average
+var hitCount = [0,0];
+const counter_desc = ["Hit: ","Hit corner: ","Average: "];
+const display_hit = {
+    total:document.getElementById("total_count_msg"),
+    corner:document.getElementById("count_msg"),
+    avg:document.getElementById("avg_msg")
+};
 
 let dvd = {
-    x: 200,
-    y: 300,
+    x: (Math.random() * 615),
+    y: (Math.random() * 425),
     xspeed: 10,
     yspeed: 10,
     img: new Image()
-};
+}
+
+display_hit.total.innerText = counter_desc[0] + hitCount[0];
+display_hit.corner.innerText = counter_desc[1] + hitCount[1];
+display_hit.avg.innerText = counter_desc[2] + parseFloat(0).toFixed(10);
 
 (function main(){
     canvas = document.getElementById("tv-screen");
     ctx = canvas.getContext("2d");
     dvd.img.src = 'dvd-logo.png';
+    //Make more randomly vector
+    var decider = Math.random();
+    if (decider<0.4&&decider>0.2) {
+        dvd.xspeed *= -1;
+    }
+    if (decider>0.3&&decider<0.5) {
+        dvd.yspeed *= -1;
+    }
+    //Draw the "tv screen" with correct resolution 
+    canvas.width  = 720;
+    canvas.height = 480;
 
-    //Draw the "tv screen"
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    pickColor();
+    hitEvent();
     update();
 })();
 
@@ -47,20 +67,29 @@ function update() {
 function checkHitBox(){
     if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
         dvd.xspeed *= -1;
-        pickColor();
-    }
-        
+        hitEvent();
+    }  
     if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
         dvd.yspeed *= -1;
-        pickColor();
+        hitEvent();
     }    
 }
 
 //Pick a random color in RGB format
-function pickColor(){
-    r = Math.random() * (254 - 0) + 0;
-    g = Math.random() * (254 - 0) + 0;
-    b = Math.random() * (254 - 0) + 0;
+function hitEvent(){
+    //Capture hitting time
+    var currentHit = Date.now();
+    display_hit.total.innerText = counter_desc[0] + (++hitCount[0]);
+    //Count as hit corner when hit 2 in 1 ms or less
+    if ((currentHit - lastHit)<=1) {
+        display_hit.corner.innerText = counter_desc[1] + (++hitCount[1]);
+    }
+    //Calculate avarage chance
+    display_hit.avg.innerText = counter_desc[2] + parseFloat(hitCount[1]/hitCount[0]).toFixed(10);
+    lastHit = currentHit;
+    r = Math.random() * (254 - 0) + 30;
+    g = Math.random() * (254 - 0) + 30;
+    b = Math.random() * (254 - 0) + 30;
 
     logoColor = 'rgb('+r+','+g+', '+b+')';
 }
